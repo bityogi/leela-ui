db08665c526efe7113a94665b6667a41855dec88
bityogi <bityogi@tutanota.com>
2019-02-07 19:40:57 -0500

    event info form more stable (wip)

--- src/components/Event/info.js
+++ src/components/Event/info.js
@@ -3,13 +3,11 @@ import Typography from '@material-ui/core/Typography';
 import Paper from '@material-ui/core/Paper';
 import Button from '@material-ui/core/Button';
 import withStyles from '@material-ui/core/styles/withStyles';
-import Grid from '@material-ui/core/Grid';
 
-import { Field, reduxForm } from 'redux-form';
-import { TextField } from 'redux-form-material-ui';
+
 
 import styles from './styles';
-import infoForm from './infoForm';
+import InfoForm from './infoForm';
 
 class Info extends Component {
 
@@ -18,53 +16,30 @@ class Info extends Component {
 
         return (
             <div className={classes.bigContainer}>
-                    <Paper className={classes.paper}>
-                      <div className={classes.topInfo}>
-                        <div>
-                          <Typography variant="subtitle1" style={{fontWeight: 'bold'}} gutterBottom>
-                            Information
-                          </Typography>
-                          <Typography variant="body2" gutterBottom>
-                            General information about this event
-                          </Typography>
-                        </div>
-                        <div>
-                        <Button variant="outlined" size="large" className={classes.outlinedButtom}>
-                          Edit
-                        </Button>
-                        </div>
-                      </div>
-                      <Grid item container xs={12}>
-                      <form>
-                      <Grid item xs={6}>
-                          <Typography style={{textTransform: 'uppercase'}} color='secondary' gutterBottom>
-                            User
-                          </Typography>
-                          <Typography variant="h5" gutterBottom>
-                            <Field name="title" component={TextField}  />
-                          </Typography>
-                        </Grid>
-                        <Grid item xs={6}>
-                          <Typography style={{textTransform: 'uppercase'}} color='secondary' gutterBottom>
-                            City
-                          </Typography>
-                          <Typography variant="h5" gutterBottom>
-                            Tokyo
-                          </Typography>
-                        </Grid>
-
-                      </form>
-                        
-                      </Grid>
-                    </Paper>
-                    </div>
+              <Paper className={classes.paper}>
+                <div className={classes.topInfo}>
+                  <div>
+                    <Typography variant="subtitle1" style={{fontWeight: 'bold'}} gutterBottom>
+                      Information
+                    </Typography>
+                    <Typography variant="body2" gutterBottom>
+                      General information about this event
+                    </Typography>
+                  </div>
+                  <div>
+                  <Button variant="outlined" size="large" className={classes.outlinedButtom}>
+                    Edit
+                  </Button>
+                  </div>
+                </div>
+                
+                <InfoForm />
+                  
+                
+              </Paper>
+            </div>
         );
     }
 }
-
-Info = reduxForm({
-  form: infoForm,
-  
-})(Info);
 
 export default withStyles(styles)(Info);
--- src/components/Event/infoForm.js
+++ src/components/Event/infoForm.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
-import { TextField } from 'redux-form-material-ui';
-import Button from '@material-ui/core/Button';
-import withStyles from '@material-ui/core/styles/withStyles';
+import { TextField, Select } from 'redux-form-material-ui';
+import Grid from '@material-ui/core/Grid';
+import MenuItem from '@material-ui/core/MenuItem';
+import Typography from '@material-ui/core/Typography';
 
-import styles from './styles';
+
+// import withStyles from '@material-ui/core/styles/withStyles';
+
+// import styles from './styles';
 
 const validate = values => {
     const errors = {}
@@ -23,19 +27,43 @@ class InfoForm extends Component {
     }
 
     render() {
-        const { classes, handleSubmit } = this.props;
+        const { handleSubmit } = this.props;
 
         return (
             <form onSubmit={handleSubmit(this.handleFormSubmit)}>
-                <div variant="outlined" className={classes.formControl}>
-                    <label>Title</label>
-                    <div>
-                        <Field name="title" component={TextField}  />
-                    </div>
-                </div>
+                <Grid item container xs={12}>
+                
+                    <Grid item xs={6}>
+                        <Typography style={{textTransform: 'uppercase'}} color='secondary' gutterBottom>
+                            Title
+                        </Typography>
+                        <Typography variant="h5" gutterBottom>
+                            <Field name="title" component={TextField}  />
+                        </Typography>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <Typography style={{textTransform: 'uppercase'}} color='secondary' gutterBottom>
+                                Location
+                            </Typography>
+                            <Typography variant="h5" gutterBottom>
+                                <Field
+                                    style={{ width:'80%', fontSize: '.9em' }}
+                                    name="location"
+                                    component={Select}
+                                    hintText="Type"
+                                    autoWidth={true}
+                                    iconStyle={{ padding: 0, margin: 0, border: '2px', width: '15px' }}
+                                    >
+                                    <MenuItem key={'Location 1'} value={1}>Location 1</MenuItem>
+                                    <MenuItem key={'Location 2'} value={2}>Location 2</MenuItem>
+                                
+                                </Field>
+                            </Typography>
+                        </Grid>
 
-              
-            </form>
+                    </Grid>
+
+                      </form>
         )
     }
 
@@ -47,4 +75,4 @@ InfoForm = reduxForm({
     warn: () => {}
 })(InfoForm)
 
-export default withStyles(styles)(InfoForm);
+export default InfoForm;
