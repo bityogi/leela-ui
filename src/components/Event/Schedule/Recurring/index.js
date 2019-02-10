a8bfe16039b233205cb77736986b74f86c50b4e9
bityogi <bityogi@tutanota.com>
2019-02-10 17:59:29 -0500

    schedule -- recurring-type -- wip

--- src/components/Event/Schedule/Recurring/index.js
+++ src/components/Event/Schedule/Recurring/index.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
+import MenuItem from '@material-ui/core/MenuItem';
+import Typography from '@material-ui/core/Typography';
+import { TextField, Select } from 'redux-form-material-ui';
 
 const validate = (values) => {
     const errors = {}
@@ -21,7 +24,24 @@ class Recurring extends Component {
         const { handleSubmit } = this.props;
         return (
             <form onSubmit={handleSubmit(this.handleFormSubmit)}>
-            
+                <Typography style={{textTransform: 'uppercase'}} color='secondary' gutterBottom>
+                    Frequency
+                </Typography>
+                <Typography variant="h5" gutterBottom>
+                    <Field
+                        style={{ width:'80%', fontSize: '.9em' }}
+                        name="frequency"
+                        component={Select}
+                        autoWidth={true}
+                        >
+                        <MenuItem key={'Daily'} value={'Daily'}>Daily</MenuItem>
+                        <MenuItem key={'Weekly'} value={'Weekly'}>Weekly</MenuItem>
+                        <MenuItem key={'Monthly'} value={'Monthly'}>Montly</MenuItem>
+                        <MenuItem key={'Yearly'} value={'Yearly'}>Yearly</MenuItem>
+                    
+                    </Field>
+                   
+                </Typography>
             </form>
         );
     }
