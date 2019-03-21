0e6495a7a7df1c2d73d8a93448c572242c79b609
bityogi <bityogi@tutanota.com>
2019-03-21 14:10:04 -0400

    allow for year to be displayed on selected value with date-time pickers

--- src/components/common/dateTimePicker.js
+++ src/components/common/dateTimePicker.js
@@ -3,7 +3,7 @@ import { DateTimePicker } from 'material-ui-pickers';
 import { isEmpty } from 'lodash'
 import moment from 'moment';
 
-const renderDateTimePicker = ({ input: { onChange, value }, label, meta: { touched, error, warning }, initialFocusedDate }) => {
+const renderDateTimePicker = ({ input: { onChange, value }, label, meta: { touched, error, warning }, initialFocusedDate, getFormatString }) => {
     const hasError = (!isEmpty(error) || !isEmpty(warning));
     const initialDateValue = isEmpty(initialFocusedDate) ? moment().startOf('hour').add(1, 'hours') : initialFocusedDate;
     
@@ -14,11 +14,14 @@ const renderDateTimePicker = ({ input: { onChange, value }, label, meta: { touch
             label={label}
             value={!value ? null : new Date(value)}
             onChange={onChange}
+            format={'yyyy/MM/dd hh:mm a'}
+            disableOpenOnEnter
             error={hasError}
             helperText={(error || warning)}
             allowKeyboardControl
             initialFocusedDate={initialDateValue}
             minutesStep={5}
+            
         />
     )
 }
