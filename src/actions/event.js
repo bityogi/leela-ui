1e812442ecbcff5fbb8391926f66979dbfbda188
bityogi <bityogi@tutanota.com>
2019-03-23 15:25:36 -0400

    normalize event data -- wip

--- src/actions/event.js
+++ src/actions/event.js
@@ -1,4 +1,5 @@
 import { submit, getFormValues } from 'redux-form';
+import { isEmpty } from 'lodash';
 
 import {
     FETCH_START,
@@ -10,13 +11,19 @@ import {
     showNotification
 } from './admin';
 
+import {
+    MEDIATYPE
+} from 'util/enums';
+
 export const submitEvent = (values) => {
     return (dispatch, getState) => {
 
         const eventValues = getFormValues('event')(getState());
-        console.log('ACTION: submitEvent -- with values: ', eventValues);
+        console.log('ACTION: received form-values: ', eventValues);
+        
         dispatch(submit('event'));
-                
+        const eventData = normalizeEventData(eventValues);
+        console.log('submitting event with data: ', eventData);        
         dispatch({
             type: FETCH_START
         });
@@ -40,3 +47,41 @@ export const submitEvent = (values) => {
         
     }
 } 
+
+function normalizeEventData(data) {
+    let eventData =  {
+        name: data.title,
+        description: data.description,
+        locationId: data.location,
+        start: data.start,
+        end: data.end,
+        media: [],
+        eventPrice: {
+            amount: data.price
+        }
+    };
+
+    if (!isEmpty(data.eventImage)) {
+        eventData.media.push({
+            mediaType: MEDIATYPE.EVENT_IMAGE,
+            path: data.EventImage.NewFileName
+        })
+    }
+    if (!isEmpty(data.altImage)) {
+        eventData.media.push({
+            mediaType: MEDIATYPE.ALTERNATE_EVENT_IMAGE,
+            path: data.altImage.NewFileName
+        })
+    }
+
+    if (data.hasSessions) {
+        eventData.sessions = data.sessions;
+    }
+
+    if (data.questions.length > 0) {
+        eventData.questions = data.questions
+    }
+
+    return eventData;
+
+}
---
+++ src/util/enums.js
@@ -0,0 +1,6 @@
+export const MEDIATYPE = Object.freeze({
+    BANNER_IMAGE : Symbol(0),
+    PROFILE_IMAGE : Symbol(1),
+    EVENT_IMAGE: Symbol(2),
+    ALTERNATE_EVENT_IMAGE: Symbol(3)
+})
